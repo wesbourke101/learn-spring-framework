0a1211c279aceca68cbcb6bb248983af5df4f273
wesbourke <wesbourkeavl@gmail.com>
2023-01-18 16:16:33 -0500

    Testing my commit skills

--- src/main/java/com/in28minutes/learnspringframework/LearnSpringFrameworkApplication.java
+++ src/main/java/com/in28minutes/learnspringframework/LearnSpringFrameworkApplication.java
@@ -8,6 +8,10 @@ public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		System.out.println("this is a test");
 	}
 
 }
+
+//add this comment to check my commit and push skills
+
