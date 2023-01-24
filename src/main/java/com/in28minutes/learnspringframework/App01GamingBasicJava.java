cff552fd62dc677e5856a4baf708e2020bb03e66
wesbourke <wesbourkeavl@gmail.com>
2023-01-24 13:59:36 -0500

    Keeping the Git up to date! added a few @beans

--- src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
+++ src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
@@ -1,15 +1,14 @@
 package com.in28minutes.learnspringframework;
 
-import com.in28minutes.learnspringframework.game.MarioGame;
 import com.in28minutes.learnspringframework.game.GameRunner;
-import com.in28minutes.learnspringframework.game.SuperContraGame;
 import com.in28minutes.learnspringframework.game.PackManGame;
+import com.in28minutes.learnspringframework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
     public static void main(String[] args) {
-        //var game = new SuperContraGame();
+        var game = new SuperContraGame();
         //var game = new MarioGame();
-        var game = new PackManGame();
+        //var game = new PackManGame();
         var gameRunner = new GameRunner(game);
         gameRunner.run();
     }
